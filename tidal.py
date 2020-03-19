b8b71d48ff4aa8294695daefb00ef45f148bdb61
Yi Jer Loh <loh.yijer@live.com>
2020-03-19 16:15:22 +0800

    added massive body, with stationary central mass

--- tidal.py
+++ tidal.py
@@ -1,15 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+G = 1
+
 class Body:
-    def __init__(self, mass, x=0, y=0, G=1):
+    def __init__(self, mass, x=0, y=0, v_x=0, v_y=0, G=G):
         self.mass = mass
         self.x = x
         self.y = y
+        self.v_x = v_x
+        self.v_y = v_y
 
 
 class TestParticle:
-    def __init__(self, velocity, mass=1, init_x=0, init_y=0, G=1):
+    def __init__(self, velocity, mass=1, init_x=0, init_y=0, G=G):
         self.mass = mass
         self.G = G
         self.x = init_x
@@ -37,54 +42,81 @@ class TestParticle:
         self.y += dt * self.v_y
 
 
-radii_and_units = [
-    (2, 12),
-    (3, 18),
-    (4, 24), 
-    (5, 30),
-    (6, 36)
-]
+# radii_and_units = [
+#     (2, 12),
+#     (3, 18),
+#     (4, 24), 
+#     (5, 30),
+#     (6, 36)
+# ]
 
-all_particles = []
+# all_particles = []
 
-G = 1
 body = Body(1)
-for (radius, num_of_units) in radii_and_units:
-    print(num_of_units)
-    thetas = np.random.rand(num_of_units) * 2 * np.pi
-    v_mag = np.sqrt(G * body.mass / radius)
-    init_x = lambda t : radius * np.cos(t)
-    init_y = lambda t : radius * np.sin(t)
-    init_v_x = lambda t : -v_mag * np.sin(t)
-    init_v_y = lambda t : v_mag * np.cos(t)
+# for (radius, num_of_units) in radii_and_units:
+#     print(num_of_units)
+#     thetas = np.random.rand(num_of_units) * 2 * np.pi
+#     v_mag = np.sqrt(G * body.mass / radius)
+#     init_x = lambda t : radius * np.cos(t)
+#     init_y = lambda t : radius * np.sin(t)
+#     init_v_x = lambda t : -v_mag * np.sin(t)
+#     init_v_y = lambda t : v_mag * np.cos(t)
 
-    particles = [TestParticle(
-        [init_v_x(theta), init_v_y(theta)], 
-        mass = 0.01, 
-        init_x = init_x(theta), 
-        init_y = init_y(theta),
-    ) for theta in thetas]
+#     particles = [TestParticle(
+#         [init_v_x(theta), init_v_y(theta)], 
+#         mass = 0.01, 
+#         init_x = init_x(theta), 
+#         init_y = init_y(theta),
+#     ) for theta in thetas]
 
-    all_particles.append(particles)
+#     all_particles.append(particles)
+
+
+# dt = 0.01
+
+# for particles_in_radius in all_particles:
+#     for particle in particles_in_radius:
+#         t_arr = []
+#         x_arr = []
+#         y_arr = []
+#         t = 0
+#         while (t < 100):
+#             particle.set_gforce(body)
+#             particle.update_speed_position(dt)
+#             t_arr.append(t)
+#             x_arr.append(particle.x)
+#             y_arr.append(particle.y)
+#             t += dt
+#         plt.plot(x_arr, y_arr)
+
+
+# plt.show()
+
+# parabolic orbit: y^2 = 4 * r_min^2 - 4 * r_min * x
+r_min = 10
+moving_mass = 1
+moving_x = -50
+moving_y = -2 * np.sqrt(r_min**2 - r_min * moving_x)
+dy_dx = r_min / np.sqrt(r_min**2 - r_min * moving_x)
+moving_v_mag = np.sqrt(2 * G * moving_mass / np.sqrt(moving_x**2 + moving_y**2))
+moving_v_y = dy_dx * moving_v_mag / np.sqrt(1 + dy_dx**2)
+moving_v_x = moving_v_y / dy_dx
+moving_body = TestParticle([moving_v_x, moving_v_y], init_x=moving_x, init_y= moving_y)
 
 dt = 0.01
+t = 0
 
-for particles_in_radius in all_particles:
-    for particle in particles_in_radius:
-        t_arr = []
-        x_arr = []
-        y_arr = []
-        t = 0
-        while (t < 100):
-            particle.set_gforce(body)
-            particle.update_speed_position(dt)
-            t_arr.append(t)
-            x_arr.append(particle.x)
-            y_arr.append(particle.y)
-            t += dt
-        plt.plot(x_arr, y_arr)
+t_arr = []
+x_arr = []
+y_arr = []
 
+while (t < 1000):
+    moving_body.set_gforce(body)
+    moving_body.update_speed_position(dt)
+    t_arr.append(t)
+    x_arr.append(moving_body.x)
+    y_arr.append(moving_body.y)
+    t += dt
 
+plt.plot(x_arr, y_arr)
 plt.show()
-
-
