3015dc198501e34f5b5315b85bb4b1d4be69a2fa
Yi Jer Loh <loh.yijer@live.com>
2020-03-19 22:21:29 +0800

    add write to file

--- tidal.py
+++ tidal.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import datetime
 
 
 G = 1
@@ -91,38 +92,58 @@ for radius, num_of_units in radii_and_units:
     particles_x_arrs.append([[]] * num_of_units)
     particles_y_arrs.append([[]] * num_of_units)
 
-while (t < 5000):
-    t_arr.append(t)
-
-    central_body.set_gforce([moving_body])
-    central_body.update_speed_position(dt)
-    central_x_arr.append(central_body.x)
-    central_y_arr.append(central_body.y)
+datetime_now = datetime.datetime.now().strftime("%H:%M:%S %Y-%m-%d")
+with open(f"{datetime_now}-position-data.csv", "w+") as file:
+    header = "central_x,central_y,moving_x,moving_y"
+    for radius, num_of_units in radii_and_units:
+        header_str = ",".join([f"particle_{radius}_{i}_x,particle_{radius}_{i}_y" for i in range(num_of_units)])
+        header += "," + header_str
     
-    moving_body.set_gforce([central_body])
-    moving_body.update_speed_position(dt)
-    moving_x_arr.append(moving_body.x)
-    moving_y_arr.append(moving_body.y)
+    file.write(header + "\n")
 
-    for i, radius_particles in enumerate(all_particles):
-        for j, particle in enumerate(radius_particles):
-            particle.set_gforce([central_body, moving_body])
-            particle.update_speed_position(dt)
-            particles_x_arrs[i][j].append(particle.x)
-            particles_y_arrs[i][j].append(particle.y)
+    count = 0
+    while (t < 5000):
+        t_arr.append(t)
 
-    t += dt
+        row = ""
+        
+        central_body.set_gforce([moving_body])
+        central_body.update_speed_position(dt)
+        # central_x_arr.append(central_body.x)
+        # central_y_arr.append(central_body.y)
+        row += str(central_body.x) + "," + str(central_body.y)
+        
+        moving_body.set_gforce([central_body])
+        moving_body.update_speed_position(dt)
+        # moving_x_arr.append(moving_body.x)
+        # moving_y_arr.append(moving_body.y)
+        row += "," + str(moving_body.x) + "," + str(moving_body.y)
 
-    if t % 5 < 10e-3:
-        plt.clf()
-        plt.scatter(moving_x_arr[-1], moving_y_arr[-1], color='r')
-        plt.scatter(central_x_arr[-1], central_y_arr[-1], color='b')
-        plt.scatter(particles_x_arrs[0][0][-1], particles_y_arrs[0][0][-1], color='g', s=5)
-        plt.ylim([-100, 100])
-        plt.xlim([-100, 100])
-        plt.pause(0.01)
+        for i, radius_particles in enumerate(all_particles):
+            for j, particle in enumerate(radius_particles):
+                particle.set_gforce([central_body, moving_body])
+                particle.update_speed_position(dt)
+                row += "," + str(particle.x) + "," + str(particle.y)
+                # particles_x_arrs[i][j].append(particle.x)
+                # particles_y_arrs[i][j].append(particle.y)
+
+        file.write(row)
+
+        t += dt
+        if count % 500 == 0:
+            # plt.clf()
+            # plt.scatter(moving_x_arr[-1], moving_y_arr[-1], color='r')
+            # plt.scatter(central_x_arr[-1], central_y_arr[-1], color='b')
+            # plt.scatter(particles_x_arrs[0][0][-1], particles_y_arrs[0][0][-1], color='g', s=5)
+            # plt.ylim([-100, 100])
+            # plt.xlim([-100, 100])
+            # plt.pause(0.01)
+            print(t)
+
+        count += 1
+        
     
-plt.title("Done")
+# plt.title("Done")
 # plt.plot(moving_x_arr, moving_y_arr, color='r')
 # plt.plot(central_x_arr, central_y_arr, color='b')
 # plt.show()
