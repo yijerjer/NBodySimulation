1dfec5c2a775c1e028d58870b4a11ef1c36745e3
Yi Jer Loh <loh.yijer@live.com>
2020-03-19 17:42:54 +0800

    added test particles

--- tidal.py
+++ tidal.py
@@ -36,80 +36,93 @@ class Body:
 
 central_body = Body([0, 0], [0, 0], mass=1)
 
-# radii_and_units = [
-#     (2, 12),
-#     (3, 18),
-#     (4, 24), 
-#     (5, 30),
-#     (6, 36)
-# ]
+radii_and_units = [
+    (2, 12),
+    (3, 18),
+    (4, 24), 
+    (5, 30),
+    (6, 36)
+]
 
-# init_x = lambda t : radius * np.cos(t)
-# init_y = lambda t : radius * np.sin(t)
-# init_v_x = lambda t : -v_mag * np.sin(t)
-# init_v_y = lambda t : v_mag * np.cos(t)
+init_x = lambda t : radius * np.cos(t)
+init_y = lambda t : radius * np.sin(t)
+init_v_x = lambda t : -v_mag * np.sin(t)
+init_v_y = lambda t : v_mag * np.cos(t)
 
-# all_particles = []
+all_particles = []
 
-# for (radius, num_of_units) in radii_and_units:
-#     print(num_of_units)
-#     thetas = np.random.rand(num_of_units) * 2 * np.pi
-#     v_mag = np.sqrt(G * central_body.mass / radius)
+for (radius, num_of_units) in radii_and_units:
+    print(num_of_units)
+    thetas = np.random.rand(num_of_units) * 2 * np.pi
+    v_mag = np.sqrt(G * central_body.mass / radius)
 
-#     particles = [Body(
-#         [init_x(theta), init_y(theta)],
-#         [init_v_x(theta), init_v_y(theta)], 
-#         mass = 0.01 
-#     ) for theta in thetas]
+    particles = [Body(
+        [init_x(theta), init_y(theta)],
+        [init_v_x(theta), init_v_y(theta)], 
+        mass = 0.01 
+    ) for theta in thetas]
 
-#     all_particles.append(particles)
-
-
-# dt = 0.01
-
-# for particles_in_radius in all_particles:
-#     for particle in particles_in_radius:
-#         t_arr = []
-#         x_arr = []
-#         y_arr = []
-#         t = 0
-#         while (t < 100):
-#             particle.set_gforce([central_body])
-#             particle.update_speed_position(dt)
-#             t_arr.append(t)
-#             x_arr.append(particle.x)
-#             y_arr.append(particle.y)
-#             t += dt
-#         plt.plot(x_arr, y_arr)
+    all_particles.append(particles)
 
 
-# plt.show()
 
 # parabolic orbit: y^2 = 4 * r_min^2 - 4 * r_min * x
-# r_min = 10
-# moving_mass = 1
-# moving_x = -50
-# moving_y = -2 * np.sqrt(r_min**2 - r_min * moving_x)
-# dy_dx = r_min / np.sqrt(r_min**2 - r_min * moving_x)
-# moving_v_mag = np.sqrt(2 * G * central_body.mass / np.sqrt(moving_x**2 + moving_y**2))
-# moving_v_y = dy_dx * moving_v_mag / np.sqrt(1 + dy_dx**2)
-# moving_v_x = moving_v_y / dy_dx
-# moving_body = Body([moving_x, moving_y], [moving_v_x, moving_v_y], mass=1)
+r_min = 20
+moving_mass = 1
+moving_x = -50
+moving_y = -2 * np.sqrt(r_min**2 - r_min * moving_x)
+dy_dx = r_min / np.sqrt(r_min**2 - r_min * moving_x)
+moving_v_mag = np.sqrt(2 * G * central_body.mass / np.sqrt(moving_x**2 + moving_y**2))
+moving_v_y = dy_dx * moving_v_mag / np.sqrt(1 + dy_dx**2)
+moving_v_x = moving_v_y / dy_dx
+moving_body = Body([moving_x, moving_y], [moving_v_x, moving_v_y], mass=1)
 
-# dt = 0.01
-# t = 0
+dt = 0.01
+t = 0
 
-# t_arr = []
-# x_arr = []
-# y_arr = []
+t_arr = []
+central_x_arr = []
+central_y_arr = []
+moving_x_arr = []
+moving_y_arr = []
+particles_x_arrs = []
+particles_y_arrs = []
+for radius, num_of_units in radii_and_units:
+    particles_x_arrs.append([[]] * num_of_units)
+    particles_y_arrs.append([[]] * num_of_units)
 
-# while (t < 1000):
-#     moving_body.set_gforce([central_body])
-#     moving_body.update_speed_position(dt)
-#     t_arr.append(t)
-#     x_arr.append(moving_body.x)
-#     y_arr.append(moving_body.y)
-#     t += dt
+while (t < 5000):
+    t_arr.append(t)
 
-# plt.plot(x_arr, y_arr)
+    central_body.set_gforce([moving_body])
+    central_body.update_speed_position(dt)
+    central_x_arr.append(central_body.x)
+    central_y_arr.append(central_body.y)
+    
+    moving_body.set_gforce([central_body])
+    moving_body.update_speed_position(dt)
+    moving_x_arr.append(moving_body.x)
+    moving_y_arr.append(moving_body.y)
+
+    for i, radius_particles in enumerate(all_particles):
+        for j, particle in enumerate(radius_particles):
+            particle.set_gforce([central_body, moving_body])
+            particle.update_speed_position(dt)
+            particles_x_arrs[i][j].append(particle.x)
+            particles_y_arrs[i][j].append(particle.y)
+
+    t += dt
+
+    if t % 5 < 10e-3:
+        plt.clf()
+        plt.scatter(moving_x_arr[-1], moving_y_arr[-1], color='r')
+        plt.scatter(central_x_arr[-1], central_y_arr[-1], color='b')
+        plt.scatter(particles_x_arrs[0][0][-1], particles_y_arrs[0][0][-1], color='g', s=5)
+        plt.ylim([-100, 100])
+        plt.xlim([-100, 100])
+        plt.pause(0.01)
+    
+plt.title("Done")
+# plt.plot(moving_x_arr, moving_y_arr, color='r')
+# plt.plot(central_x_arr, central_y_arr, color='b')
 # plt.show()
