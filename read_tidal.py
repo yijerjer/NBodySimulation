1a6633ae7a688e81caa5727d90011ec29b662142
Yi Jer Loh <loh.yijer@live.com>
2020-03-21 12:20:15 +0800

    added loops for experiments

--- read_tidal.py
+++ read_tidal.py
@@ -3,69 +3,108 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import time
 
-color_and_units = [
-    ('g', 12),
-    ('c', 18),
-    ('m', 24), 
-    ('y', 30),
-    ('k', 36)
+radii_units_colors = [
+    (2, 120, 'g'),
+    (3, 180, 'c'),
+    (4, 240, 'm'), 
+    (5, 300, 'y'),
+    (6, 360, 'k')
 ]
 
-# using pandas because 36.56730818748474 vs 110.98377823829651
-print("Reading file...")
-positions = pd.read_csv("ac_positions_rmin20.csv", delimiter=",")
-print("File read.")
+for val in [2.2, 2.6, 3.0]:
+    dists = np.zeros([6, 3])
+    for turn in [1, 2, 3]:
 
-
-snapshot_rows = np.linspace(0, len(positions)/2 - 1, 100, dtype=int)
-snapshot_positions = positions.iloc[snapshot_rows,:]
-
-for idx, row in snapshot_positions.iterrows():
-    time = row[0]
-    central_x = row[1]
-    central_y = row[2]
-    moving_x = row[3]
-    moving_y = row[4]
-    test_particles_positions = row[5:]
-    all_test_particles_x = [coord for idx, coord in enumerate(test_particles_positions) if idx % 2 == 0]
-    all_test_particles_y = [coord for idx, coord in enumerate(test_particles_positions) if idx % 2 == 1]
-
-    test_particles_xs = []
-    test_particles_ys = []
-    index = 0
-    for color, num in color_and_units:
-        test_particles_xs.append(all_test_particles_x[index:index + num])
-        test_particles_ys.append(all_test_particles_y[index:index + num])
-        index += num
-
-    plt.clf()
-    plt.scatter(central_x, central_y, color='b')
-    plt.scatter(moving_x, moving_y, color='r')
-    for idx, (color, num) in enumerate(color_and_units):
-        plt.scatter(test_particles_xs[idx], test_particles_ys[idx], s=1, color=color)
+        # using pandas because 36.56730818748474 vs 110.98377823829651
+        print("Reading file...")
+        positions = pd.read_csv(f"positions_mass{val}_{turn}.csv", delimiter=",")
+        print("File read.")
         
-    plt.xlim([-100, 100])
-    plt.ylim([-100, 100])
-    plt.pause(0.01)
+        print("VAL" + str(val))
 
-final_row = snapshot_positions.iloc[-1]
-time = final_row[0]
-central_x = final_row[1]
-central_y = final_row[2]
-moving_x = final_row[3]
-moving_y = final_row[4]
-test_particles_positions = final_row[5:]
-all_test_particles_x = [coord for idx, coord in enumerate(test_particles_positions) if idx % 2 == 0]
-all_test_particles_y = [coord for idx, coord in enumerate(test_particles_positions) if idx % 2 == 1]
+        # snapshot_rows = np.linspace(len(positions)/13, len(positions)/5 - 1, 5, dtype=int)
+        snapshot_rows = [6000]
+        snapshot_positions = positions.iloc[snapshot_rows,:]
 
-test_particles_xs = []
-test_particles_ys = []
-index = 0
-for color, num in color_and_units:
-    test_particles_xs.append(np.array(all_test_particles_x[index:index + num]))
-    test_particles_ys.append(np.array(all_test_particles_y[index:index + num]))
-    index += num
+        # fig, axs = plt.subplots(6, 1, figsize=(7, 20))
+        # axs_count = 0
 
-for idx in range(len(test_particles_xs)):
-    distance_to_central = np.sqrt(np.square(test_particles_xs[idx] - central_x) + np.square(test_particles_ys[idx] - central_y))
-    print(distance_to_central)
+        # for idx, row in snapshot_positions.iterrows():
+        #     time = row[0]
+        #     central_x = row[1]
+        #     central_y = row[2]
+        #     moving_x = row[3]
+        #     moving_y = row[4]
+        #     test_particles_positions = row[5:]
+        #     all_test_particles_x = [coord for idx, coord in enumerate(test_particles_positions) if idx % 2 == 0]
+        #     all_test_particles_y = [coord for idx, coord in enumerate(test_particles_positions) if idx % 2 == 1]
+
+        #     test_particles_xs = []
+        #     test_particles_ys = []
+        #     index = 0
+        #     for (radius, num, color) in radii_units_colors:
+        #         test_particles_xs.append(all_test_particles_x[index:index + num])
+        #         test_particles_ys.append(all_test_particles_y[index:index + num])
+        #         index += num
+
+            # plt.clf()
+            # plt.scatter(central_x, central_y, color='b')
+            # plt.scatter(moving_x, moving_y, color='r')
+            # for idx, (radius, num, color) in enumerate(radii_units_colors):
+            #     plt.scatter(test_particles_xs[idx], test_particles_ys[idx], s=1, color=color)
+                
+            # plt.xlim([-100, 100])
+            # plt.ylim([-100, 100])
+            # plt.show()
+            # plt.pause(0.01)
+
+            # axs[axs_count].scatter(central_x, central_y, color='b')
+            # axs[axs_count].scatter(moving_x, moving_y, color='r')
+            # for unit_idx, (color, num) in enumerate(radii_units_colors):
+            #     axs[axs_count].scatter(test_particles_xs[unit_idx], test_particles_ys[unit_idx], s=1, color=color)
+                
+            # axs[axs_count].set_xlim([-100, 100])
+            # axs[axs_count].set_ylim([-100, 100])
+            # axs[axs_count].set_title(f"t = {time}, index = {idx}")
+            # axs_count += 1
+
+        # fig.savefig("c_positions.pdf")
+
+        final_row = snapshot_positions.iloc[-1]
+        time = final_row[0]
+        central_x = final_row[1]
+        central_y = final_row[2]
+        moving_x = final_row[3]
+        moving_y = final_row[4]
+        test_particles_positions = final_row[5:]
+        all_test_particles_x = [coord for idx, coord in enumerate(test_particles_positions) if idx % 2 == 0]
+        all_test_particles_y = [coord for idx, coord in enumerate(test_particles_positions) if idx % 2 == 1]
+
+        test_particles_xs = []
+        test_particles_ys = []
+        index = 0
+        for radius, num, color in radii_units_colors:
+            test_particles_xs.append(np.array(all_test_particles_x[index:index + num]))
+            test_particles_ys.append(np.array(all_test_particles_y[index:index + num]))
+            index += num
+
+        for idx, (radius, num, color) in enumerate(radii_units_colors):
+            distance_to_central = np.sqrt(np.square(test_particles_xs[idx] - central_x) + np.square(test_particles_ys[idx] - central_y))
+            disturbed = [dist for dist in distance_to_central if dist > (radius + 0.5) or dist < (radius - 0.5)]
+            print(f"Disturbed particles for radius {radius}: {len(disturbed)}/{num}")
+
+            dists[idx][turn - 1] = len(disturbed)
+            
+        distance_to_moving = np.sqrt(np.square(np.array(all_test_particles_x) - moving_x) + np.square(np.array(all_test_particles_y) - moving_y))
+        print(len(distance_to_moving))
+
+        moving_orbit = [dist for dist in distance_to_moving if dist < 8]
+        print(len(moving_orbit))
+        dists[5][turn - 1] = len(moving_orbit)
+    
+    print(dists)
+    dists_average = [np.average(dist) for dist in dists]
+    dists_std = [np.std(dist) for dist in dists]
+    print(dists_average)
+    print(dists_std / np.sqrt(3))
+    print()
--- tidal.py
+++ tidal.py
@@ -27,6 +27,8 @@ class Body:
             force_mag = self.G * body.mass * self.mass / distance**2
             self.f_x += force_mag * d_x / distance
             self.f_y += force_mag * d_y / distance
+        
+        return self
 
     def update_speed_position(self, dt):
         self.v_x += dt * self.f_x / self.mass
@@ -34,85 +36,88 @@ class Body:
         self.x += dt * self.v_x
         self.y += dt * self.v_y
 
-
-for val in [50]:
-    central_body = Body([0, 0], [0, 0], mass=1)
-
-    radii_and_units = [
-        (2, 12),
-        (3, 18),
-        (4, 24), 
-        (5, 30),
-        (6, 36)
-    ]
-
-    init_x = lambda t : radius * np.cos(t)
-    init_y = lambda t : radius * np.sin(t)
-    init_v_x = lambda t : -v_mag * np.sin(t)
-    init_v_y = lambda t : v_mag * np.cos(t)
-
-    all_particles = []
-
-    for (radius, num_of_units) in radii_and_units:
-        print(num_of_units)
-        thetas = np.random.rand(num_of_units) * 2 * np.pi
-        v_mag = np.sqrt(G * central_body.mass / radius)
-
-        particles = [Body(
-            [init_x(theta), init_y(theta)],
-            [init_v_x(theta), init_v_y(theta)], 
-            mass = 0.01 
-        ) for theta in thetas]
-
-        all_particles.append(particles)
+        return self
 
 
+for turn in [1, 2, 3]:
+    for val in [0.4, 0.8, 1.2]:
+        central_body = Body([0, 0], [0, 0], mass=1)
 
-    # parabolic orbit: y^2 = 4 * r_min^2 - 4 * r_min * x
-    r_min = val
-    moving_mass = 1
-    moving_x = -50
-    moving_y = -2 * np.sqrt(r_min**2 - r_min * moving_x)
-    dy_dx = r_min / np.sqrt(r_min**2 - r_min * moving_x)
-    moving_v_mag = np.sqrt(2 * G * central_body.mass / np.sqrt(moving_x**2 + moving_y**2))
-    moving_v_y = dy_dx * moving_v_mag / np.sqrt(1 + dy_dx**2)
-    moving_v_x = moving_v_y / dy_dx
-    moving_body = Body([moving_x, moving_y], [moving_v_x, moving_v_y], mass=1)
+        radii_and_units = [
+            (2, 120),
+            (3, 180),
+            (4, 240), 
+            (5, 300),
+            (6, 360)
+        ]
 
-    dt = 0.1
-    t = 0
+        init_x = lambda t : radius * np.cos(t)
+        init_y = lambda t : radius * np.sin(t)
+        init_v_x = lambda t : -v_mag * np.sin(t)
+        init_v_y = lambda t : v_mag * np.cos(t)
 
-    # datetime_now = datetime.datetime.now().strftime("%H:%M:%S %Y-%m-%d")
-    with open(f"positions_rmin{val}.csv", "w+") as file:
-        header = "time,central_x,central_y,moving_x,moving_y"
-        for radius, num_of_units in radii_and_units:
-            header_str = ",".join([f"particle_{radius}_{i}_x,particle_{radius}_{i}_y" for i in range(num_of_units)])
-            header += "," + header_str
-        
-        file.write(header + "\n")
+        all_particles = []
 
-        count = 0
-        while (t < 3000):
-            row = str(t) + ","
+        for (radius, num_of_units) in radii_and_units:
+            print(num_of_units)
+            thetas = np.random.rand(num_of_units) * 2 * np.pi
+            v_mag = np.sqrt(G * central_body.mass / radius)
+
+            particles = [Body(
+                [init_x(theta), init_y(theta)],
+                [init_v_x(theta), init_v_y(theta)], 
+                mass = 0.01 
+            ) for theta in thetas]
+
+            all_particles.append(particles)
+
+
+
+        # parabolic orbit: y^2 = 4 * r_min^2 - 4 * r_min * x
+        r_min = 20
+        moving_mass = 1
+        moving_x = -50
+        moving_y = -2 * np.sqrt(r_min**2 - r_min * moving_x)
+        dy_dx = r_min / np.sqrt(r_min**2 - r_min * moving_x)
+        moving_v_mag = np.sqrt(2 * G * central_body.mass / np.sqrt(moving_x**2 + moving_y**2))
+        moving_v_y = dy_dx * moving_v_mag / np.sqrt(1 + dy_dx**2)
+        moving_v_x = moving_v_y / dy_dx
+        moving_body = Body([moving_x, moving_y], [moving_v_x, moving_v_y], mass=val)
+
+        dt = 0.1
+        t = 0
+
+        # datetime_now = datetime.datetime.now().strftime("%H:%M:%S %Y-%m-%d")
+        with open(f"positions_mass{val}_{turn}.csv", "w+") as file:
+            header = "time,central_x,central_y,moving_x,moving_y"
+            for radius, num_of_units in radii_and_units:
+                header_str = ",".join([f"particle_{radius}_{i}_x,particle_{radius}_{i}_y" for i in range(num_of_units)])
+                header += "," + header_str
             
-            central_body.set_gforce([moving_body])
-            central_body.update_speed_position(dt)
-            row += str(central_body.x) + "," + str(central_body.y)
-            
-            moving_body.set_gforce([central_body])
-            moving_body.update_speed_position(dt)
-            row += "," + str(moving_body.x) + "," + str(moving_body.y)
+            file.write(header + "\n")
 
-            for i, radius_particles in enumerate(all_particles):
-                for j, particle in enumerate(radius_particles):
-                    particle.set_gforce([central_body, moving_body])
-                    particle.update_speed_position(dt)
-                    row += "," + str(particle.x) + "," + str(particle.y)
+            count = 0
+            while (t < 700):
+                row = str(t) + ","
+                
+                central_body.set_gforce([moving_body])
+                central_body.update_speed_position(dt)
+                row += str(central_body.x) + "," + str(central_body.y)
+                
+                moving_body.set_gforce([central_body])
+                moving_body.update_speed_position(dt)
+                row += "," + str(moving_body.x) + "," + str(moving_body.y)
 
-            file.write(row + "\n")
+                for i, radius_particles in enumerate(all_particles):
+                    radius_particles = [part.set_gforce([central_body, moving_body]) for part in radius_particles]
+                    radius_particles = [part.update_speed_position(dt) for part in radius_particles]
+                    particle_positions = [str(part.x) + "," + str(part.y) for part in radius_particles]
+                    row += "," + ",".join(particle_positions)
 
-            t += dt
-            if count % 500 == 0:
-                print(t)
+                file.write(row + "\n")
 
-            count += 1
+                t += dt
+                if count % 500 == 0:
+                    print(t)
+
+                count += 1
